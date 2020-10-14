cc849c1bba5689258972472e4f64719d44972f01
piayonl <piayonl@yahoo.com>
2020-10-14 06:12:30 -0400

    Updates

---
+++ precache-manifest.6e6cfde2e7a2b21ba37e7d7ed1367dbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad0965070f345de1fdd1da0d80c9ad28",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "faac95d0ce286c18abed",
+    "url": "/ReactPortfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "3177b6f7568742f234cb",
+    "url": "/ReactPortfolio/static/js/2.478772e0.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/ReactPortfolio/static/js/2.478772e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faac95d0ce286c18abed",
+    "url": "/ReactPortfolio/static/js/main.cefa7500.chunk.js"
+  },
+  {
+    "revision": "0114f5eceb8464d5d956",
+    "url": "/ReactPortfolio/static/js/runtime-main.5b3ce1ae.js"
+  }
+]);
